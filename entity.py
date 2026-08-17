e9395b4a55d6d56bfedbe8bcab8ae529ee8d452d
Prajjwal <prajjwal.2907@gmail.com>
2026-08-17 12:19:10 +0530

    First commit

---
+++ entity.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import copy
+from typing import Tuple, TypeVar, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game_map import GameMap
+
+T = TypeVar("T", bound="Entity")
+
+class Entity:
+    """
+    A generic object to represent player, enemies, items
+    """
+
+    def __init__(self, x: int = 0, y: int = 0, char: str = "?", color: tuple[int, int, int] = (255, 255, 255), name: str = "<Unnamed>", blocks_movement: bool = False):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+
+    def spawn(self: T, game_map: GameMap, x: int, y: int) -> T:
+        """Spawn a copy of this instance at the given location."""
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        game_map.entities.add(clone)
+        return clone
+    
+    def move(self, dx: int, dy: int) -> None:
+        self.x += dx
+        self.y += dy
